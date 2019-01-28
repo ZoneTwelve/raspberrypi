92f3a5227068520624afadecf89d26f90559d4d2
ZoneTwelve <zonetwelve159@gmail.com>
2019-01-28 21:25:05 +0800

    its workgit add .!

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-mkdir /tmp/nodejs&&cd /tmp/nodejs
-wget https://nodejs.org/dist/latest-v10.x/node-v10.15.0-linux-armv6l.tar.gz -O /tmp/nodejs
-tar -xzf /tmp/nodejs/node-*.tar.gz
-sudo cp -R /tmp/nodejs/node*/* /usr/local/
+mkdir /tmp/nodejs && mkdir /tmp/nodejs/install
+wget https://nodejs.org/dist/latest-v10.x/node-v10.15.0-linux-armv6l.tar.gz -O /tmp/nodejs/node.tar.gz && tar -C /tmp/nodejs/install -xzf /tmp/nodejs/node.tar.gz && sudo cp -R /tmp/nodejs/install/* /usr/local/
 
 
