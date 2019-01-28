4cc201c203f54d71107e0d2e8fc6cf8d32e6b9bd
ZoneTwelve <zonetwelve159@gmail.com>
2019-01-28 21:07:42 +0800

    test qwq

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 mkdir /tmp/nodejs&&cd /tmp/nodejs
 wget https://nodejs.org/dist/latest-v10.x/node-v10.15.0-linux-armv6l.tar.gz -O /tmp/nodejs
-tar -xzf node-*.tar.gz
-cd node*
-sudo cp -R * /usr/local/
+tar -xzf /tmp/nodejs/node-*.tar.gz
+sudo cp -R /tmp/nodejs/node*/* /usr/local/
 
 
