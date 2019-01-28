dca9ac79bcb44c7f9a5c337c669f26b260055537
ZoneTwelve <zonetwelve159@gmail.com>
2019-01-28 21:06:09 +0800

    增加wget檔案放置路徑

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 mkdir /tmp/nodejs&&cd /tmp/nodejs
-wget https://nodejs.org/dist/latest-v10.x/node-v10.15.0-linux-armv6l.tar.gz
+wget https://nodejs.org/dist/latest-v10.x/node-v10.15.0-linux-armv6l.tar.gz -O /tmp/nodejs
 tar -xzf node-*.tar.gz
-cd node*&&sudo cp -R * /usr/local/
+cd node*
+sudo cp -R * /usr/local/
 
 
