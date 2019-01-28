07a3c9379831efd30198bb005c337ca4b28afd63
ZoneTwelve <zonetwelve159@gmail.com>
2019-01-28 21:30:43 +0800

    sorry, i forgot the folder

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /tmp/nodejs && mkdir /tmp/nodejs/install
-wget https://nodejs.org/dist/latest-v10.x/node-v10.15.0-linux-armv6l.tar.gz -O /tmp/nodejs/node.tar.gz && tar -C /tmp/nodejs/install -xzf /tmp/nodejs/node.tar.gz && sudo cp -R /tmp/nodejs/install/* /usr/local/
+wget https://nodejs.org/dist/latest-v10.x/node-v10.15.0-linux-armv6l.tar.gz -O /tmp/nodejs/node.tar.gz && tar -C /tmp/nodejs/install -xzf /tmp/nodejs/node.tar.gz && sudo cp -R /tmp/nodejs/install/node*/* /usr/local/
 
 
